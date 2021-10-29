b3ddedddfbdf63a4ac218e451cd28b2b698f7465
John-Mark Sackett <jmsackett91@gmail.com>
2021-10-29 18:19:46 -0400

    added my mutations and inputs into my typeDefs file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -24,5 +24,25 @@ type Query {
 }
 
 type Mutation {
+login(email: String!, password: String!): Auth
+addUser(username: String!, email: String!, password: String!): Auth
+saveBook(input: BookInput!): User
+removeBook(bookId: String!): User
+}
 
-}`
+input BookInput {
+    bookId: String
+    authors: [String]
+    title: String
+    description: String
+    image: String
+    link: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+`;
+
+module.exports = typeDefs;
